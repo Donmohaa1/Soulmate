9af16bee56af224894507b30b742c80b6bebb1ea
Donmohaa1 <alaricdemahom11@gmail.com>
2020-06-26 19:47:37 +0300

    add files via upload
    landing page

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SoulMate</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="css/layout.css">
+</head>
+<body>
+<div id="layout">
+  <div id="header">
+    <div id="logo"><a href="index.php"><img src="images/logo_1.gif" alt="" /></a></div>
+  </div>
+  <div id="body_container">
+    <div id="body_container_inner">
+      <div id="menu">
+        <ul>
+          <li class="first"><a class="current" href="index.php">HOME</a></li>
+          <li><a href="about_us.php">ABOUT US</a></li>
+          <li><a href="partner.php">FIND PARTNER</a></li>
+          <li><a href="login.php">LOGIN</a></li>
+          <li><a href="register.php">REGISTER</a></li>
+          <li><a href="quotes.php">LOVE QUOTES</a></li>
+          <li><a href="contact_us.php">CONTACT US</a></li>
+        </ul>
+      </div>
+      <div class="banner"><a href="#"><img src="images/banner.gif" alt="" /></a></div>
+      <div class="find_more">
+        <p>Find New Partners, Experience Romance &amp; Find Love!<br />
+          <span><a href="register.php">Register Now</a> or <span><a href="login.php">Log In</a> now and take a sneak peak at all Singles.</span></span></p>
+      </div>
+      <div id="footer">
+        <div class="footer_link">
+          <ul style="color:#FFf;">
+            Copyright (c) Soul Mate. All rights reserved.</a>.
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
